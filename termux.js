596d927c84bc25e10db50ff71cf207c6ecf49a84
Rup1 <lenglain@gmail.com>
2020-02-23 15:47:45 -0500

    changes

---
+++ termux.js
@@ -0,0 +1,29 @@
+const { startServer } = require('termux-appium');
+const DEFAULT_HOST = "localhost";
+const DEFAULT_PORT = 4884;
+(async function main () {
+  return await startServer(DEFAULT_PORT, DEFAULT_HOST);
+})();
+
+const wd = require('wd');
+
+var browser = wd.promiseChainRemote({
+    host: 'localhost',
+    port: 4884
+});
+
+const defaultCaps = {
+    deviceName: 'Termux',
+    platformName: 'Android',
+    newCommandTimeout: 1800,
+    appPackage: 'com.instagram.android',
+    appActivity: 'com.instagram.android.activity.MainTabActivity'
+};
+
+(async function runTest() {
+    await browser.init(defaultCaps);
+    await browser.sleep(5000);
+    await browser.waitForElementById("com.instagram.android:id/log_in_button", 1000).click();
+    await browser.quit();
+
+})()
