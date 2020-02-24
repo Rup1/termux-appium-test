96c9673efe0624fd788db548c1002fa30f3f4c59
Rup1 <lenglain@gmail.com>
2020-02-24 16:04:11 -0500

    console.log

--- termux.js
+++ termux.js
@@ -1,32 +1,33 @@
-const { startServer } = require('termux-appium');
-const DEFAULT_HOST = "localhost";
-const DEFAULT_PORT = 4884;
+// const { startServer } = require('termux-appium');
+// const DEFAULT_HOST = "localhost";
+// const DEFAULT_PORT = 4884;
 console.log("anything??");
-(async function main () {
-    console.log('starting server....');
-  return await startServer(DEFAULT_PORT, DEFAULT_HOST);
-})();
 
-const wd = require('wd');
+// (async function main () {
+//     console.log('starting server....');
+//   return await startServer(DEFAULT_PORT, DEFAULT_HOST);
+// })();
 
-var browser = wd.promiseChainRemote({
-    host: 'localhost',
-    port: 4884
-});
+// const wd = require('wd');
 
-const defaultCaps = {
-    deviceName: 'Termux',
-    platformName: 'Android',
-    newCommandTimeout: 1800,
-    appPackage: 'com.instagram.android',
-    appActivity: 'com.instagram.android.activity.MainTabActivity'
-};
+// var browser = wd.promiseChainRemote({
+//     host: 'localhost',
+//     port: 4884
+// });
 
-(async function runTest() {
-    console.log("running runTest...");
-    await browser.init(defaultCaps);
-    await browser.sleep(5000);
-    await browser.waitForElementById("com.instagram.android:id/log_in_button", 1000).click();
-    await browser.quit();
+// const defaultCaps = {
+//     deviceName: 'Termux',
+//     platformName: 'Android',
+//     newCommandTimeout: 1800,
+//     appPackage: 'com.instagram.android',
+//     appActivity: 'com.instagram.android.activity.MainTabActivity'
+// };
 
-})()
+// (async function runTest() {
+//     console.log("running runTest...");
+//     await browser.init(defaultCaps);
+//     await browser.sleep(5000);
+//     await browser.waitForElementById("com.instagram.android:id/log_in_button", 1000).click();
+//     await browser.quit();
+
+// })()
